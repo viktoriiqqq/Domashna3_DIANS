da507b0b69fe3a23d01b84ae2ae83e51050a8b79
viktoriiqqq <viktorija.panova@students.finki.ukim.mk>
2022-12-22 12:19:17 +0100

    prototype

---
+++ exchange_currency/js/stores.js
@@ -0,0 +1,14 @@
+let stores = [{
+    "grad": "Скопје",
+    "link": "5758-13907",
+    },
+    {
+        "grad": "Охрид",
+        "link": "5758-13907", 
+    },
+    {
+        "grad": "Kavadarci",
+        "link": <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23930.260416448713!2d21.993448601853107!3d41.43309037262173!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x135665abe7b5dee1%3A0x62e75a59d0e6e1a8!2sKavadarci!5e0!3m2!1sen!2smk!4v1671229517566!5m2!1sen!2smk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>, 
+    },
+    ]
+    
